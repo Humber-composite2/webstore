f75840813b32b541ed37bce06f6f4fbfb846b318
Ivandi Santoso <ivandisantoso@Vahns-MacBook-Pro.local>
2019-12-06 12:35:56 -0500

    Coding started

--- js/index.js
+++ js/index.js
@@ -1,187 +1,217 @@
 // GLOBAL VARIABLES
 let tes = document.getElementById(`test`);
 
+const settings = {
+  notifySeatsRemaining: 5,
+  coursesPerPage: 3,
+  imagePath: 'img/'
+}
+
+const shoppingCart = [
+  {courseid: 101, qty: 1}
+];
+
 const allProducts =[
   { //0
-    id : `101`,
-    img : `https://source.unsplash.com/random/200x200`,
+    id : 101,
+    image : `Tshirt-01.png`,
     name: `Champion Tshirt`,
-    // category: `Tshirt`,
+    brand: `Champion`,
     colour: `Red`,
     size: `M`,
-    rating: `2.4`,
-    price: `39`
+    rating: 2,
+    available: 10,
+    price: 39
   },{ //1
-    id : `102`,
-    img : `https://source.unsplash.com/random/201x200`,
+    id : 102,
+    image : `Tshirt-02.png`,
     name: `Nike Tshirt`,
-    // category: `Tshirt`,
+    brand: `Nike`,
     colour: `Red`,
     size: `S`,
-    rating: `3.4`,
-    price: `59`
+    rating: 3,
+    available: 5,
+    price: 59
   },{ //2
-    id : `103`,
-    img : `https://source.unsplash.com/random/202x200`,
+    id : 103,
+    image : `Tshirt-03.png`,
     name: `Adidas Tshirt`,
-    // category: `Tshirt`,
+    brand: `Adidas`,
     colour: `White`,
     size: `L`,
-    rating: `3.8`,
-    price: `69`
+    rating: 4,
+    available: 7,
+    price: 69
   },{ //3
-    id : `104`,
-    img : `https://source.unsplash.com/random/203x200`,
+    id : 104,
+    image : `Tshirt-04.png`,
     name: `Fila Tshirt`,
-    // category: `Tshirt`,
+    brand: `Fila`,
     colour: `Blue`,
     size: `XL`,
-    rating: `1.4`,
-    price: `49`
+    rating: 1,
+    available: 12,
+    price: 49
   },{ //4
-    id : `105`,
-    img : `https://source.unsplash.com/random/204x200`,
+    id : 105,
+    image : `Tshirt-05.png`,
     name: `Puma Tshirt`,
-    // category: `Tshirt`,
+    brand: `Puma`,
     colour: `Black`,
     size: `XL`,
-    rating: `4.6`,
-    price: `49`
+    rating: 5,
+    available: 11,
+    price: 49
   },{ //5
-    id : `106`,
-    img : `https://source.unsplash.com/random/205x200`,
+    id : 106,
+    image : `Tshirt-06.png`,
     name: `Champion Jacket`,
-    // category: `Jacket`,
+    brand: `Champion`,
     colour: `Grey`,
     size: `M`,
-    rating: `2.4`,
-    price: `139`
+    rating: 2,
+    available: 2,
+    price: 139
   },{ //6
-    id : `107`,
-    img : `https://source.unsplash.com/random/206x200`,
+    id : 107,
+    image : `Tshirt-07.png`,
     name: `Nike Jacket`,
-    // category: `Jacket`,
+    brand: `Nike`,
     colour: `Red`,
     size: `S`,
-    rating: `4.4`,
-    price: `259`
+    rating: 4,
+    available: 8,
+    price: 259
   },{ //7
-    id : `108`,
-    img : `https://source.unsplash.com/random/207x200`,
+    id : 108,
+    image : `Tshirt-08.png`,
     name: `Adidas Jacket`,
-    // category: `Jacket`,
+    brand: `Adidas`,
     colour: `White`,
     size: `L`,
-    rating: `4.8`,
-    price: `279`
+    rating: 5,
+    available: 9,
+    price: 279
   },{ //8
-    id : `109`,
-    img : `https://source.unsplash.com/random/208x200`,
+    id : 109,
+    image : `Tshirt-09.png`,
     name: `Fila Jacket`,
-    // category: `Jacket`,
+    brand: `Fila`,
     colour: `Blue`,
     size: `XS`,
-    rating: `3.4`,
-    price: `149`
+    rating: 3,
+    available: 10,
+    price: 149
   },{ //9
-    id : `110`,
-    img : `https://source.unsplash.com/random/209x200`,
+    id : 110,
+    image : `Tshirt-10.png`,
     name: `Puma Jacket`,
-    // category: `Jacket`,
+    brand: `Puma`,
     colour: `White`,
     size: `XL`,
-    rating: `4.6`,
-    price: `199`
+    rating: 5,
+    available: 10,
+    price: 199
   },{ //10
-    id : `111`,
-    img : `https://source.unsplash.com/random/210x200`,
+    id : 111,
+    image : `Tshirt-11.png`,
     name: `Champion Pants`,
-    // category: `Pants`,
+    brand: `Champion`,
     colour: `Blue`,
     size: `M`,
-    rating: `2.4`,
-    price: `89`
+    rating: 2,
+    available: 10,
+    price: 89
   },{ //11
-    id : `112`,
-    img : `https://source.unsplash.com/random/211x200`,
+    id : 112,
+    image : `Tshirt-12.png`,
     name: `Nike Pants`,
-    // category: `Pants`,
+    brand: `Nike`,
     colour: `Grey`,
     size: `M`,
-    rating: `3.4`,
-    price: `129`
+    rating: 3,
+    available: 10,
+    price: 129
   },{ //12
-    id : `113`,
-    img : `https://source.unsplash.com/random/212x200`,
+    id : 113,
+    image : `Tshirt-13.png`,
     name: `Adidas Pants`,
-    // category: `Pants`,
+    brand: `Adidas`,
     colour: `White`,
     size: `XS`,
-    rating: `1.4`,
-    price: `89`
+    rating: 1,
+    available: 10,
+    price: 89
   },{ //13
-    id : `114`,
-    img : `https://source.unsplash.com/random/213x200`,
+    id : 114,
+    image : `Tshirt-14.png`,
     name: `Fila Pants`,
-    // category: `Pants`,
+    brand: `Fila`,
     colour: `Black`,
     size: `M`,
-    rating: `2.9`,
-    price: `69`
+    rating: 3,
+    available: 10,
+    price: 69
   },{ //14
-    id : `115`,
-    img : `https://source.unsplash.com/random/214x200`,
+    id : 115,
+    image : `Tshirt-15.png`,
     name: `Puma Pants`,
-    // category: `Pants`,
+    brand: `Puma`,
     colour: `White`,
     size: `L`,
-    rating: `2`,
-    price: `69`
+    rating: 2,
+    available: 10,
+    price: 69
   },{ //15
-    id : `116`,
-    img : `https://source.unsplash.com/random/215x200`,
+    id : 116,
+    image : `Tshirt-16.png`,
     name: `Champion Shorts`,
-    // category: `Shorts`,
+    brand: `Champion`,
     colour: `White`,
     size: `L`,
-    rating: `0`,
-    price: `19`
+    rating: 0,
+    available: 10,
+    price: 19
   },{ //16
-    id : `117`,
-    img : `https://source.unsplash.com/random/216x200`,
+    id : 117,
+    image : `Tshirt-17.png`,
     name: `Nike Shorts`,
-    // category: `Shorts`,
+    brand: `Nike`,
     colour: `Red`,
     size: `L`,
-    rating: `2.2`,
-    price: `39`
+    rating: 2,
+    available: 10,
+    price: 39
   },{ //17
-    id : `118`,
-    img : `https://source.unsplash.com/random/217x200`,
+    id : 118,
+    image : `Tshirt-18.png`,
     name: `Adidas Shorts`,
-    // category: `Shorts`,
+    brand: `Adidas`,
     colour: `White`,
     size: `L`,
-    rating: `0`,
-    price: `23`
+    rating: 0,
+    available: 10,
+    price: 23
   },{ //18
-    id : `119`,
-    img : `https://source.unsplash.com/random/218x200`,
+    id : 119,
+    image : `Tshirt-19.png`,
     name: `Fila Shorts`,
-    // category: `Shorts`,
+    brand: `Fila`,
     colour: `Blue`,
     size: `XL`,
-    rating: `2.1`,
-    price: `19`
+    rating: 2,
+    available: 10,
+    price: 19
   },{ //19
-    id : `120`,
-    img : `https://source.unsplash.com/random/219x200`,
+    id : 120,
+    image : `Tshirt-20.png`,
     name: `Puma Shorts`,
-    // category: `Shorts`,
+    brand: `Puma`,
     colour: `Grey`,
     size: `S`,
-    rating: `4.7`,
-    price: `49`
+    rating: 5,
+    available: 10,
+    price: 49
   }
 ];
 
@@ -190,34 +220,147 @@ function test(){
   alert(`test`);
 }
 
-const getProductAsHtmlString = (prod) =>{
-  return `
-  <article class="product">
-  <header class="img-pro">
-      <img src=${prod.img} alt="">
-  </header>
+
+// Sorting order function
+const loadProductsByOrder = (arrToSort, criteria) => {
+
+  const sortedProducts = arrToSort.slice(); // clone the array
+
+  if (criteria == 'priceLow') {
+    // Smallest to largest
+    sortedProducts.sort((a, b) => a.price - b.price);
+  } else if (criteria == 'priceHigh') {
+    // Largest to smallest
+    sortedProducts.sort((a, b) => b.price - a.price);
+  } else if (criteria == 'nameAsc') {
+    // Largest to smallest
+    sortedProducts.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (criteria == 'nameDesc') {
+    // Largest to smallest
+    sortedProducts.sort((a, b) => b.name.localeCompare(a.name));
+  }
+
+  return sortedProducts; // new array
+}
+
+// Filter
+const runTheFilter = theForm => {
+
+  const brandToSearch = theForm.elements.brandName.value;
+  const nameToSearch = theForm.elements.courseName.value;
+  // const ratingToSearch = theForm.elements.rating.value;
+
+  console.log(theForm.elements)
+
+  // Filter all courses in multiple
+  const filteredCourses = allCourses
+    .filter(c => c.name.toLowerCase().includes( nameToSearch.trim().toLowerCase() ))  // filter by name
+    .filter(c => c.brand == brandToSearch || brandToSearch == "all")         // filter by category
+    // .filter(c => `${c.rating}` == ratingToSearch || ratingToSearch == "all");        // filter by semester
+
+  renderCoursesFromArray(filteredCourses);
+}
+
+
+
+// EVENT HANDLER FUNCTIONS **************
+
+
+// When the filter form itself is submit...
+const filterTheCourses = event => {
+  event.preventDefault(); // Prevent the submit form refreshing
+  runTheFilter(event.target);
+}
+
+// When a filter form component is submit...
+const submitTheFilterForm = event => {
+  //document.getElementById('filtersForm').submit();
+  runTheFilter(event.target.form);
+}
+
+// When sorting, ensure the filters run first (which leads to a render)
+const sortTheCourses = event => {
+  runTheFilter(document.getElementById('filtersForm'));
+}
+
+// FUNCTIONS THAT BUILD OUR VIEW **************
+
+/*  Function: getCourseAsHtmlString
+    Parameters: course:Object
+    Return: String of HTML (<article>)
+    Description: Allows our courses to be built using a template (for map())  */
+const getProductAsHtmlString = (product) =>{
+  let callout = ``;
+  let soldout = ``;
+  let register = `<button type="button" class="course-register" data-courseid="${product.id}">Register</button>`;
+  if (product.available <= 0) {
+    callout = `<small class="callout">Sold out</small>`;
+    soldout = `soldout`;
+    register = ``;
+  } else if (product.available < settings.notifySeatsRemaining) {
+    callout = `<small class="callout urgent">Limited seats remaining</small>`;
+  }
   
-  <form>
-      <h3>${prod.name}</h3>
-      <p>Here is a shot of this product that might entice a user to click and add it to their cart.</p>
-      <ul>
-        <li><label>${prod.colour}</label></li>
-        <li><label>${prod.size}</label></li>
-        <li><label>${prod.rating}</label></li>
-        <li><label>${prod.price}</label></li>
+  return `
+  <article class="course ${(product.brand) ? `cat-${product.brand}` : ''} ${soldout}">
+  <header class="img-pro">
+  <img src="${settings.imagePath + product.image}" alt="${product.name}">
+  </header>
+ 
+      <h3>${product.name} ${callout}</h3>
+      <ul class="product-info">
+        <li><label>${product.colour}</label></li>
+        <li><label>${product.size}</label></li>
+        <li><label>${product.rating}</label></li>
+        <li><label>${product.price}</label></li>
+        <li><label>${product.brand}</label></li>
       </ul>
-  </form>
+      ${register}
 </article>
   `;
 }
 
 const renderProductsFromArray = (arr) => {
-  document.getElementById(`products`).innerHTML = arr.map(getProductAsHtmlString).join(``);
-  let res = `result`;
-  if (arr.length == 1){
-    res = `result`
+
+//   document.getElementById(`products`).innerHTML = arr.map(getProductAsHtmlString).join(``);
+//   let res = `result`;
+//   if (arr.length == 1){
+//     res = `result`
+//   }
+//   document.getElementById(`numResuts`).innerHTML = `(${arr.length} ${res})`;
+// }
+
+// 1. SORT!!!
+const sortBy = document.getElementById('sortOrder').value;    // dropdown value
+arr = loadProductsByOrder(arr, sortBy);  // sort the courses, reassign the new Array
+  // 2. PAGINATE
+    // How many pages do we need? 
+    //  totalNumCourses / coursesPerPage
+    //    Round this^ up to the next integer
+    
+    // TEST EXAMPLE:  10 courses / 3 coursesPerPage = 4 pages required
+    //    Page 1:   0, 1, 2
+    //    Page 2:   3, 4, 5
+    //    Page 3:   6, 7, 8
+    //    Page 4:   9
+
+    // Therefor...
+    // Index of the first course on each page: 
+    //    firstIndexOnThisPage = (pageNum - 1) * coursesPerPage
+    // Index of the last course on each page:
+    //    lastIndexOnThisPage = firstIndexOnThisPage + coursesPerPage
+    //      (Remember that slice() excludes the last index.)
+
+
+  // 3. BUILD OUTPUT
+  if (arr.length > 0) {
+    document.getElementById('products').innerHTML = arr.map(getProductAsHtmlString).join('\n'); // Print courses  
+  } else {
+    document.getElementById('products').innerHTML = 'Sorry, no matching results.' // Woops!
   }
-  document.getElementById(`numResuts`).innerHTML = `(${arr.length} ${res})`;
+
+  document.getElementById('numResults').innerHTML = `(${arr.length} ${(arr.length == 1) ? 'result' : 'results'})`; // Display number of total results
+  
 }
 
 // EXE
@@ -225,6 +368,24 @@ const renderProductsFromArray = (arr) => {
 //START
 window.addEventListener(`load`, () => {
   renderProductsFromArray(allProducts);
-})
+
+ // The course elements
+ renderCoursesFromArray(allCourses);
+
+ // UI elements
+ document.getElementById('productView').addEventListener('click', toggleProductView);
+//  document.getElementById('fallCourses').addEventListener('click', loadCoursesFromTerm);
+ document.getElementById('products').addEventListener('click', handleClickOfProducts);
+ document.getElementById('sortOrder').addEventListener('change', sortTheProducts);
+
+ // The filter form
+ document.getElementById('filtersForm').addEventListener('submit', filterTheProducts);
+
+ // The filter form components
+ document.getElementById('productName').addEventListener('input', submitTheFilterForm);
+ document.getElementById('brandName').addEventListener('change', submitTheFilterForm);
+ document.querySelectorAll('[name="rating"]').forEach(radbtn => radbtn.addEventListener('change', submitTheFilterForm));
+
+});
 
 //document.addEventListener("click",test);
